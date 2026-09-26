a2d0320d9e1647571a9f2a789f3bb22c1bf1cbaf
Damla Güneş <gunsandamla@gmail.com>
2026-09-26 17:57:48 +0300

    runpy render adresi eklendi

--- run.py
+++ run.py
@@ -2,7 +2,6 @@ from flask_cors import CORS
 from app import create_app
 
 app = create_app()
-CORS(app, origins=["[https://gunesdamla.wixstudio.com](https://gunesdamla.wixstudio.com)"])
-
+CORS(app, origins=["https://gunesdamla.wixstudio.com", "https://benimki-qr6m.onrender.com"])
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
